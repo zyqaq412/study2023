d3e06f9fea336d389b0eba7e1eec17637a393750
zxwyhzy <2791517764@qq.com>
2023-03-12 12:56:00 +0800

    逻辑删除字段加@TableLogic注解

--- 用户中心/userCenter/src/main/java/com/hzy/usercenter/entity/User.java
+++ 用户中心/userCenter/src/main/java/com/hzy/usercenter/entity/User.java
@@ -1,5 +1,6 @@
 package com.hzy.usercenter.entity;
 
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,31 +19,32 @@ import java.util.Date;
 @NoArgsConstructor
 @TableName("user")
 public class User  {
-    //用户id@TableId
+    // 用户id@TableId
     private Long id;
 
-    //昵称
+    // 昵称
     private String username;
-    //登录账号
+    // 登录账号
     private String useraccount;
-    //头像
+    // 头像
     private String avatarurl;
-    //性别
+    // 性别
     private Integer gender;
-    //密码
+    // 密码
     private String userpassword;
-    //电话
+    // 电话
     private String phone;
-    //邮箱
+    // 邮箱
     private String email;
-    //用户状态：0正常
+    // 用户状态：0正常
     private Integer userstatus;
-    //创建时间，数据插入时间
+    // 创建时间，数据插入时间
     private Date createtime;
-    //更新时间，数据更新时间
+    // 更新时间，数据更新时间
     private Date updatetime;
 
-    //是否删除：0未删除
+    // 是否删除：0未删除
+    @TableLogic
     private Integer isdelete;
 
 
